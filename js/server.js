8948fb0c0f06e9ff4d9491b97e67d98105bb37a3
grisha <argon7155@gmail.com>
2022-04-28 00:51:32 +0300

    add

--- js/server.js
+++ js/server.js
@@ -1,7 +1,10 @@
 const http = require('http')
+const fs = require('fs')
 const server = http.createServer((req,res)=>{
-    console.log('Hello world');
-    res.end('Hello world')
+    const index = req.url === 'css/style.css'
+        ?fs.readFileSync('../css/style.css')
+        :fs.readFileSync('../index.html')
+    res.end(index)
 })
 
 server.listen(3000)
